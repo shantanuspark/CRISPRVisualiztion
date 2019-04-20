6e5f37ba7a3dacc1025e554be44bf819ecf37574
Shantanu <shantanuspark@gmail.com>
2019-04-19 20:28:37 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,88 @@
+import os
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
+import time
+import random
+import subprocess
+import os.path
+import json
+from flask import jsonify
+from Bio.Seq import Seq
+from Bio import motifs,SeqIO
+from dna_features_viewer import GraphicFeature, GraphicRecord, CircularGraphicRecord
+
+
+app = Flask(__name__)
+
+def randomDigits(digits):
+    lower = 10**(digits-1)
+    upper = 10**digits - 1
+    return random.randint(lower, upper)
+
+@app.route('/')
+def render_index():
+   return render_template('index.html')
+	
+@app.route('/uploader', methods = ['POST'])
+def upload_file():
+   try:
+      filename = str(randomDigits(9))+".json"
+      f = request.files['seqfile'].save(secure_filename(filename))
+   except:
+      return 'File Upload Error', 400
+   with open(filename,"r") as f:
+      data = json.load(f)
+   return jsonify(data)
+
+
+@app.route('/generate_image/<file_name>', methods = ['POST'])
+def create_webLogo(file_name):
+    repeats = request.get_json()
+    m = motifs.create(list(repeats))
+    m.weblogo('static/logos/'+str(file_name)+'.png')
+    return jsonify('{"success":1}')
+
+@app.route('/generate_dna_struct/<file_name>', methods = ['POST'])
+def create_dna_structure(file_name):
+   results = request.get_json()
+   features = []
+   for i, spacerRepeat in enumerate(results['spacerRepeats']):
+      features.append(GraphicFeature(start=spacerRepeat['position'], end=spacerRepeat['position']+len(spacerRepeat['repeat']), 
+                  strand=+1, color="#cffccc", label="Repeat_"+str(i+1)))
+      if 'spacer' in spacerRepeat:
+         features.append(GraphicFeature(start=spacerRepeat['position']+len(spacerRepeat['repeat'])+1, 
+         end=spacerRepeat['position']+len(spacerRepeat['repeat'])+spacerRepeat['lengths'][1], strand=+1, color="#ccccff",
+                   label="Spacer_"+str(i+1)))
+   record = GraphicRecord(sequence_length=results['length'], features=features)
+   record = record.crop((results['spacerRepeats'][0]['position']-50, 
+      results['spacerRepeats'][len(results['spacerRepeats'])-1]['position']+
+      len(results['spacerRepeats'][len(results['spacerRepeats'])-1]['repeat'])+50))
+   ax, _ = record.plot(figure_width=10)
+   ax.figure.savefig('static/logos/'+str(file_name)+'.png', bbox_inches='tight')
+   return jsonify('{"success":1}')
+		
+@app.route('/generate_structure/<file_name>', methods = ['POST'])
+def create_2dStructure(file_name):
+   sequence = request.get_json()
+   with open("uploaded_sequences/"+file_name+".fasta", "w") as output_handle:
+      output_handle.write(">repeat\n"+sequence['seq'])
+
+   if os.path.isfile('uploaded_sequences/'+file_name+'.fasta'):
+      print('file found', 'uploaded_sequences/'+file_name+'.fasta')
+      subprocess.call(['C:\Program Files\RNAstructure6.1\exe\Fold', 'uploaded_sequences/'+file_name+'.fasta', 'uploaded_sequences/'+file_name+'.ct',
+       '--DNA', '--loop' , '30', '--maximum', '20', '--percent', '10', '--temperature', '310.15', '--window', '3'])
+   else:
+      raise Exception()
+
+   if os.path.isfile('uploaded_sequences/'+file_name+'.ct'):
+      subprocess.call(['C:\Program Files\RNAstructure6.1\exe\draw', 'uploaded_sequences/'+file_name+'.ct', 'static/logos/'+file_name+'.svg',
+       '--svg', '-n' , '1'])
+   else:
+      raise Exception()
+
+   return jsonify('{"success":1}')
+
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=80, debug=True)
+  
---
+++ static/js/main.js
@@ -0,0 +1,287 @@
+var crisprData;
+var fileID;
+$(document).ready(function () {
+    $('.progress').hide();
+    $("#findBtn").click(function () {
+        //Clear result section and show progress bar
+        $('#findBtn').addClass('disabled');
+        $('#results').html("");
+        $('.progress-bar').css('width', '50%');
+        $('.progress-bar').html('Getting the json data');
+        $('.progress').show();
+
+        // Upload sequence file
+        var fd = new FormData($("#fileinfo")[0]);
+        // filename = $("#fileinfo");
+        
+        $.ajax({
+            url: '/uploader',
+            type: 'POST',
+            data: fd,
+            success: function (data) {
+                $('.progress-bar').html('Creating Visualization');
+                $('.progress-bar').css('width', '90%');
+                displayResult(data);
+            },
+            error: function (data) {
+                displayError('Sorry, some unexpected error occured during file upload!');
+            },
+            cache: false,
+            contentType: false,
+            processData: false
+        });
+
+    });
+
+
+    $('#inputGroupFile01').on('change', function () {
+        //get the file name
+        var fileName = $(this).val();
+        //replace the "Choose a file" label
+        $(this).next('.custom-file-label').html(fileName);
+    })
+
+    $('#exampleModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+
+        $('#modal-content').html(`
+        <div class="text-center">
+            <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        `);
+        
+        var modal = $(this)
+
+        switch(button.data('modal-type')){
+        case("Repeat Weblogo"):
+            var id = button.data('ciripr-id') // Extract info from data-* attributes
+            var repeats = []
+            crisprData[id]['spacerRepeat'].forEach(function(value){
+                repeats.push(value['repeat']);
+            });
+            $.ajax({
+                url: '/generate_image/' + fileID,
+                type: 'POST',
+                data: JSON.stringify(repeats),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (d) {
+                    modal.find('#modal-content').html(`
+                    <div class="text-center">
+                        <img src="static/logos/`+fileID+`.png" class="img-fluid" />
+                    </div>
+                    `);
+                },
+                failure: function (d){
+                    console.log('error')
+                }
+            });
+            break;
+        case("Sequence Structure"):
+            var crSeq = button.data('ciripr-seq') // Extract info from data-* attributes
+            var rand = Math.floor(Math.random() * 1000);
+            $.ajax({
+                url: '/generate_structure/' + fileID+'_'+rand,
+                type: 'POST',
+                data: JSON.stringify({"seq":crSeq}),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (d) {
+                    modal.find('#modal-content').html(`
+                    <div class="text-center">
+                    <img src="static/logos/`+fileID+`_`+rand+`.svg" type="image/svg+xml" />
+                    <a target="_blank" href="https://blast.ncbi.nlm.nih.gov/Blast.cgi?CMD=Put&QUERY=`+crSeq+`&PROGRAM=blastn&FORMAT_TYPE=html&DATABASE=nr" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Blast on NCBI</a>
+                    </div>                    
+                    `);
+                },
+                failure: function (d){
+                    console.log('error')
+                }
+            }); 
+            break;
+        case("CRISPR Structure"):
+            var id = button.data('ciripr-id') // Extract info from data-* attributes
+            var repeats = []
+            requestJson = {}
+            requestJson['spacerRepeats'] = crisprData[id]['spacerRepeat'];
+            requestJson['length'] = button.data('crispr-length')
+            var rand = Math.floor(Math.random() * 1000);
+            $.ajax({
+                url: '/generate_dna_struct/' + fileID+'_'+rand,
+                type: 'POST',
+                data: JSON.stringify(requestJson),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (d) {
+                    modal.find('#modal-content').html(`
+                    <div class="text-center">
+                        <img src="static/logos/`+fileID+`_`+rand+`.png" class="img-fluid" />
+                    </div>
+                    `);
+                },
+                failure: function (d){
+                    console.log('error')
+                }
+            });
+        break;
+        }
+        modal.find('.modal-title').text(button.data('modal-type'))
+    })
+
+});
+
+function displayError(msg) {
+    $('#results').html(`
+    <div class="alert alert-danger" role="alert">
+    `+ msg + `
+    </div>
+    `);
+    $('#findBtn').removeClass('disabled');
+    $("#fasta-sequence").val("");
+    $('.progress').hide();
+}
+
+function displayResult(data) {
+    $("#findBtn").removeClass('disabled');
+    $("#fasta-sequence").val("");
+    $('.progress').hide();
+    $('#results').html(createResults(data));
+}
+
+function createResults(data) {
+    content = `
+    <div class="alert alert-light" role="alert">
+    <h5 class="alert-heading">Organism </h5>` +
+        data['organism'] +
+        `<hr>
+    <p class="mb-0">
+    <span class="badge badge-pill badge-secondary"> No. of Bases: ` + data['noOfBases'] + `</span>
+    <span class="badge badge-pill badge-light">No. of valid CRISPRs: `+ data['validCrisprs'] + `</span>
+    <span class="badge badge-pill badge-secondary">No. of questionable CRISPRs: `+ (data['noOfCRISPRs'] - data['validCrisprs']) + `</span>
+    <span class="badge badge-pill badge-light">Time taken to find CRISPRs: `+ Math.floor(data['timeTaken']) + `ms</span>
+    </p>
+    </div>
+    <div id="accordion">`
+    content += getTables(true, data);
+    content+=`<br/>`
+    content+=getTables(false, data);
+
+    content += `</div></div></div>`
+
+    return content;
+}
+
+function getTables(isValid, data) {
+    table_content = `
+    <div class="card">
+    <div class="card-header" id="headingTwo">
+    <h5 class="mb-0"> `+ ((isValid)?(`<button class="btn" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
+    aria-controls="collapseOne">
+    Valid CRISPRs <span class="badge badge-dark">` +data['validCrisprs']+ `</span>
+</button>`):(`
+    <button class="btn collapsed" data-toggle="collapse" data-target="#collapseTwo"
+                                aria-expanded="false" aria-controls="collapseTwo">
+        Questionable CRISPRs <span class="badge badge-dark"> `+ (data['noOfCRISPRs'] - data['validCrisprs']) + ` </span>
+    </button>` ))+`
+    </h5>
+    </div>`+(isValid?`<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">`:
+    `<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">` )
+    +`<div class="card-body">
+    <table class="table table-hover">
+    <thead>
+    <tr>
+    <th scope="col">ID</th>
+    <th scope="col">Start Position</th>
+    <th scope="col">End Position</th>
+    <th scope="col">Average Repeat Length</th>
+    <th scope="col">Average Spacer Length</th>
+    </tr>
+    </thead><tbody>`
+    data['CRISPRs'].forEach(function (value, i) {
+        if (value['isValid'] == isValid) {
+            table_content += `
+        <tr class='resultRow'>
+        <a href='#`+ i + `'>
+        <th scope="row"><a class="text-secondary" href='#`+ i + `'>CRISPR_`+ (i + 1) + `</a></th>
+        <td>`+ value['startRange'] + `</td>
+        <td>`+ value['endRange'] + `</td>
+        <td>`+ value['avgRLength'] + `</td>
+        <td>`+ value['avgSLength'] + `</td>
+        </a>
+        </tr>`
+        }
+    });
+    table_content += `</tbody></table>`
+    crisprData = data['CRISPRs']
+    data['CRISPRs'].forEach(function (value, i) {
+        if (value['isValid'] == isValid) {
+            table_content += `
+        
+        <div id="`+ i + `" class="card border-secondary mb-3">
+                                <div class="card-header">CRISPR `+ (i + 1) + ` : <span class="text-muted">Range: ` + value['startRange'] + ` - ` + value['endRange'] + ` </span>
+                                </div>
+                                <div class="card-body text-secondary">
+                                    <p class="card-text">
+                                        <p class="mb-0">
+                                            <span class="alert alert-secondary" role="alert">No. of Repeats: `+ value['noOfRepeats'] + `</span>
+                                            <span class="alert alert-secondary" role="alert">Average Spacer Length: `+ value['avgSLength'] + `</span>
+                                            <span class="alert alert-secondary" role="alert">Average Repeat Length: `+ value['avgRLength'] + `</span>
+                                        </p>
+                                        <br />
+                                        <div class="table-responsive">
+                                        <table class="table table-sm table-bordered text-center">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col" colspan="3" >REPEAT</th>
+                                                    <th scope="col" colspan="3" class="table-secondary">SPACER</th>
+                                                </tr>
+                                            </thead>
+                                            <tr>
+                                                <th scope="col">REPEAT Sequence</th>
+                                                <th scope="col">POSITION</th>
+                                                <th scope="col">LENGTH</th>
+                                                <th scope="col" class="table-secondary">SPACER Sequence</th>
+                                                <th scope="col" class="table-secondary">POSITION</th>
+                                                <th scope="col" class="table-secondary">LENGTH</th>
+                                            </tr>
+                                            <tbody>`
+            value['spacerRepeat'].forEach(function (value, i) {
+                if (value['spacer']) {
+                    table_content += `<tr>
+                            <td scope="row"><button type="button" class="btn btn-sm" data-toggle="modal" data-target="#exampleModal" data-ciripr-seq="`+value['repeat'].trim()+`
+                            " data-modal-type="Sequence Structure" data-file-name=`+fileID+`>`+ value['repeat'] + `</button></td>
+                            <td>`+ value['position'] + `</td>
+                            <td>`+ value['lengths'][0] + `</td>
+                            <td class="table-secondary"><button type="button" class="btn btn-sm" data-toggle="modal" data-target="#exampleModal" data-ciripr-seq="`+value['spacer'].trim()+`
+                            " data-modal-type="Sequence Structure" data-file-name=`+fileID+`>`+ value['spacer'] + `</button></td>
+                            <td class="table-secondary">`+ (value['position']+value['lengths'][0]) + `</td>
+                            <td class="table-secondary">`+ value['lengths'][1] + `</td>
+                        </tr>`;
+                } else {
+                    table_content += `<tr>
+                            <td scope="row"><button type="button" class="btn btn-sm" data-toggle="modal" data-target="#exampleModal" data-ciripr-seq="`+value['repeat'].trim()+`
+                            " data-modal-type="Sequence Structure" data-file-name=`+fileID+`>`+ value['repeat'] + `</button></td>
+                            <td>`+ value['position'] + `</td>
+                            <td>`+ value['repeat'].length + `</td>
+                            <td class="table-secondary">-</td>
+                            <td class="table-secondary">-</td>
+                            <td class="table-secondary">-</td>
+                        </tr>`;
+                }
+            });
+            table_content += `</tbody></table></div>
+        <button type="button" class="btn btn-secondary " data-toggle="modal"
+            data-target="#exampleModal" data-ciripr-id="`+i+`" data-modal-type="Repeat Weblogo" data-file-name=`+fileID+`>Repeat weblogo</button>
+            <button type="button" class="btn btn-secondary " data-toggle="modal"
+            data-target="#exampleModal" data-ciripr-id="`+i+`" data-crispr-length="`+data['noOfBases']+`" data-modal-type="CRISPR Structure" data-file-name=`+fileID+`>CRISPR Structure</button>
+            </p>
+            </div>
+        </div>`}
+    });
+
+    table_content += `</div></div></div>`
+
+    return table_content
+}
